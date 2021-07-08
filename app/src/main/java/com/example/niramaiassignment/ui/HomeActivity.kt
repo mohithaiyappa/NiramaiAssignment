d73b0d7f61484d16db3d2afbf3643f818ce2eb23
Mohith <Mohithaiyappa229@gmail.com>
2021-07-08 14:47:09 +0530

    :zap: Create Project Floating Action Button Added

--- app/src/main/java/com/example/niramaiassignment/ui/HomeActivity.kt
+++ app/src/main/java/com/example/niramaiassignment/ui/HomeActivity.kt
@@ -3,10 +3,12 @@ package com.example.niramaiassignment.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.niramaiassignment.databinding.ActivityMainBinding
 import com.example.niramaiassignment.ui.updatedetails.UpdateDetailsActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomeActivity : AppCompatActivity() {
 
@@ -14,6 +16,8 @@ class HomeActivity : AppCompatActivity() {
 
     private val projectRecyclerView: RecyclerView
         get() = binding.recyclerView
+    private val fab: FloatingActionButton
+        get() = binding.floatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +29,11 @@ class HomeActivity : AppCompatActivity() {
         projectRecyclerView.layoutManager = LinearLayoutManager(this)
         projectRecyclerView.adapter = ProjectsAdapter(context = this, projects = emptyList())
 
+        fab.setOnClickListener(this::onFabClicked)
+    }
+
+    // Listeners
+    private fun onFabClicked(view: View){
         val i = Intent(this, UpdateDetailsActivity::class.java)
         startActivity(i)
     }
